2bd69205c50ae5ede076093eb25bd8263f36bd1b
jhoeljp <jhoel.jjpr@gmail.com>
2023-06-06 18:41:03 +0300

    completed populating SQL database

--- OTE/populate.py
+++ OTE/populate.py
@@ -2,37 +2,37 @@ import psycopg2
 import psycopg2.extras
 import config 
 from OTE import *
-from datetime import datetime
-from time import sleep
 
+#Establish connection with postgres Data base 
 connection = psycopg2.connect(host=config.DB_HOST,database=config.DB_NAME,user=config.DB_USER,password=config.DB_PASSWORD)
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-# cursor.execute('SELECT * FROM ote_records')
-# cursor.execute('DELETE FROM ote_records')
 
-# rows = cursor.fetchall()
-
-# for _ in rows:
-#     print(_)
-
+#Data Class 
 OTE_obj = OTE_Data()
 
 #Pandas Data Frame 
-# ote_df = OTE_obj.get_historical_data()
-ote_df = OTE_obj.get_tomorrow_data()
+ote_df = OTE_obj.get_historical_data()
+# ote_df = OTE_obj.get_yesterdays_data()
 
-# print(all_data)
-
+#parse through entire dataset 
 for _,row in ote_df.iterrows():
-    try:
-        values = f"({row['Day_hour']},'{row['Price']}','{row['Amount']}','{row['Export']}','{row['Import']}','{row['Balance']}','{row['Date'].date().isoformat()}')"
-        query = "INSERT INTO ote_records (day_hour,price,amount,export,import,balance,record_date) VALUES " + values + ';'
-        print(query)
-        
-        cursor.execute(query)
 
-        sleep(2)
+    try:
+        #convert date type from typestamp 
+        final_date = row['Date'].date().isoformat()
+
+        #form query 
+        cursor.execute("""INSERT INTO ote_records (day_hour,price,amount,export,import,balance,record_date)
+                        VALUES (%s, %s, %s,%s,%s,%s, %s );
+                        """,(row['Day_hour'],row['Price'],row['Amount'],row['Export'],row['Import'],row['Balance'],final_date))
+        
+        print("Insert into ote_records")
 
     except Exception as e:
-        print(f'INSERT QUERY FAILED! {e}')
-    # print(query)
+        print(f'INSERT QUERY FAILED! {e}') 
+
+#commit changes to database 
+connection.commit()
+
+#close database connection
+connection.close()        
