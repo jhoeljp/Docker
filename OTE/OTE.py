3f226de6bb961205fdf6306c3bfe345f6ffea916
jhoeljp <jhoel.jjpr@gmail.com>
2023-06-24 10:07:44 +0300

    refactor (fetcher): simplify OTE class
    constructor for specifying date
    get date function to reduce duplicated code
    assume historical download default for main constructor
    specify parameters and return types of functions
    add time sensitive check for requesting correct url

--- OTE/OTE.py
+++ OTE/OTE.py
@@ -7,34 +7,56 @@ from io import BytesIO
 1. Download historical data for the month
 2. Download todays day 
 '''
+URL = "https://www.ote-cr.cz/pubweb/attachments"
 
 class OTE_Data:
 
-    def __init__(self):
-        self.base_url = "https://www.ote-cr.cz/pubweb/attachments"
+    def __init__(self) -> None:
+        self.base_url = URL
+
+        #first date known for dateset 
+        start_date = datetime(2022, 6, 8)
+
+        #historical date range is default
+        self.get_date(start_date.day, start_date.month, start_date.year)
+        
+    def __init__(self, day: int, month: int, year: int):
+        self.base_url = URL
+        self.get_date(day,month,year)
+    
+    def get_date(self,day: int, month: int, year: int) -> None:
+        #OTE document get updated everyday 15:00 Central European Time
+        date_today = datetime.now()
+
+        if date_today.hour < 15:
+            date_today.day -= 1
+
+        self.end_date = datetime(date_today.year,date_today.month,date_today.day)
+        self.start_date = datetime(year,month,day)
 
     # Function to download and process the xls file
-    def get_data(self,date):
-        # Create the URL
+    def get_data(self, date: datetime) -> pd.DataFrame:
         
+        # Create the URL
         url = f"{self.base_url}/01/{date.year}/month{str(date.month).zfill(2)}/day{str(date.day).zfill(2)}/DT_{str(date.day).zfill(2)}_{str(date.month).zfill(2)}_{date.year}_CZ.xls"
         print(url)
         
         # Download the xls file
         response = requests.get(url)
+
         if response.status_code != 200:
-            print(f"Failed to download data. URL: {url}")
-            return None
+            print(f"Error {response.status_code} | Failed to download data . URL: {url}")
+            response.raise_for_status()
 
         # Load the data into a pandas DataFrame
-        # data = pd.read_excel(BytesIO(response.content), header=4, usecols="A:F", nrows=27)
-        data = pd.read_excel(BytesIO(response.content), header=4, nrows=27)
+        data = pd.read_excel(BytesIO(response.content), header=4, usecols="A:F", nrows=27)
+        # data = pd.read_excel(BytesIO(response.content), header=4, nrows=27)
         data['Date'] = date
 
         return data
 
     # Function to download data
-    def download_data(self,start_date, end_date):
+    def download_data(self, start_date : datetime, end_date : datetime) -> pd.DataFrame:
         # Create a date range
         dates = pd.date_range(start_date, end_date)
 
@@ -47,17 +69,6 @@ class OTE_Data:
             data = self.get_data(date)
             if data is not None:
                 all_data = pd.concat([all_data, data])
-
-        #re-arrange columns for dataframe until date 08.06.2022 to most updated column format 
-        if date <= datetime(2022, 6, 8):
-
-            df = df[['Hodina', 'Cena (EUR/MWh)', 'Množství\n(MWh)','Saldo DT\n(MWh)','Export\n(MWh)','Import\n(MWh)']]
-
-            #rename column to up to date convention 
-            all_data.rename(columns = {'Hodina':'Day_hour', 'Cena (EUR/MWh)':'Price', 'Množství\n(MWh)':'Amount','Saldo DT\n(MWh)':'Balance','Export\n(MWh)':'Export','Import\n(MWh)':'Import'}, inplace = True)
-
-        #from 09.06.2023 ignore 
-        else:
             
             #Rename columns from czech to english
             all_data.rename(columns = {'Hodina':'Day_hour', 'Cena (EUR/MWh)':'Price', 'Množství\n(MWh)':'Amount','Saldo':'Balance'}, inplace = True)
@@ -66,36 +77,22 @@ class OTE_Data:
 
         return all_data
 
-    #Function to download date from starting date to todays date
-    def get_historical_data(self):
-        # Define the start and end dates
-        start_date = datetime(2019, 1, 1)
-        
+    #Download data from starting date to todays date
+    def get_df_data(self) -> pd.DataFrame:
 
-        date_obj = datetime.now()
-        end_year, end_day, end_month = date_obj.year , date_obj.day, date_obj.month
-        end_date = datetime(end_year, end_month, end_day)  # for example, June 5, 2023
+        return self.download_data(self.start_date,self.end_date)
+    
+    #Download data from yesterday
+    def get_yesterdays_data(self) -> pd.DataFrame:
 
-        #delete this 
-        end_date = datetime(2019, 6, 1)
-        
-        return self.download_data(start_date,end_date)
+        start_date = self.end_date
+        start_date = start_date.day -1 
 
-    #Function to download todays date #after 3 pm czech time 
-    def get_tomorrow_data(self):
-        # Define the start and end dates
+        return self.download_data(start_date,self.end_date)
+    
+    #Download all existing data available 
+    def get_historical_data(self) -> pd.DataFrame:
+        #first date known 
+        start_date = datetime(2022, 6, 8)
 
-        date_obj = datetime.now()
-        end_year, end_day, end_month = date_obj.year , date_obj.day, date_obj.month
-        end_date = datetime(end_year, end_month, end_day+1)
-
-        return self.download_data(end_date,end_date)
-    def get_yesterdays_data(self):
-        # Define the start and end dates
-        #end_date == start_date
-
-        date_obj = datetime.now()
-        end_year, end_day, end_month = date_obj.year , date_obj.day, date_obj.month
-        end_date = datetime(end_year, end_month, end_day-1)
-
-        return self.download_data(end_date,end_date)
+        return self.download_data(start_date,self.end_date)
