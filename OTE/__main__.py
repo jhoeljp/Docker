6e77c57e97169961f781278ef1d6347e86222275
jhoeljp <jhoel.jjpr@gmail.com>
2023-08-12 17:56:32 +0300

    fix(extract dates): earliest date june 18, 2021

--- OTE/__main__.py
+++ OTE/__main__.py
@@ -2,20 +2,18 @@ from Postgres_db import *
 from OTE import OTE_Data
 from pandas import DataFrame
 
-def get_historical_data(OTE_data_obj: OTE_Data) -> DataFrame:
-    return OTE_data_obj.get_historical_data()
-
-def get_yesterday_data(OTE_data_obj: OTE_Data) -> DataFrame:
-    return OTE_data_obj.get_yesterdays_data()
-
 if __name__ == '__main__': 
      
     try:
         #Pandas Data Frame
         OTE_data_obj = OTE_Data()
+        
 
         #OTE start date
-        OTE_data_obj.set_start_date(day=1,month=4,year=2023)
+        OTE_data_obj.set_start_date(day=16,month=6,year=2021)
+        OTE_data_obj.set_end_date(day=18,month=6,year=2021)
+        # OTE_data_obj.set_start_date(day=1,month=8,year=2023)
+        OTE_data_obj.print_start_and_end()
 
         #Download data
         ote_df = OTE_data_obj.get_historical_data()
